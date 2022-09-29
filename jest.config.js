4c907854d14895bb6b2b5689c800fa9b4ebc5393
Ulises Antonio <ukisessg@gmail.com>
2022-09-29 03:41:05 -0500

    Fixed eslint errors in testing files

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,23 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  dir: './'
+  dir: './src'
 })
 
-// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const customJestConfig = {
+  moduleDirectories: [
+    'node_modules',
+    'src'
+  ],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/pages/$1'
   },
+  modulePaths: ['<rootDir>'],
+  rootDir: './',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom'
 }
