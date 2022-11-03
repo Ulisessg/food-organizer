6d3e386d9c142472826aac18c7954630ebde257a
Ulises Antonio <ukisessg@gmail.com>
2022-11-02 21:46:03 -0600

    Units Of Measure CRU created

---
+++ src/controllers/food_organizer_crud/unitsOfMeasureCRUD.ts
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable camelcase */
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from 'lib/prisma'
+import { response } from 'controllers/response'
+import unitOfMeasureValidations from 'models/unitOfMeasureValidations'
+import { units_of_measure } from '@prisma/client'
+
+export const createUOM = async (
+  req: CreateUOM,
+  res: NextApiResponse<response<units_of_measure>>
+): Promise<void> => {
+  try {
+    const { abbreviation, creation_date, name, uomt_id } = req.body
+    unitOfMeasureValidations({
+      abbreviation,
+      creationDate: creation_date as unknown as string,
+      name,
+      uomtId: uomt_id
+    })
+    const result = await prisma.units_of_measure.create({
+      data: {
+        abbreviation: abbreviation.toLocaleLowerCase(),
+        creation_date,
+        name: name.toLocaleLowerCase(),
+        uomt_id
+      }
+    })
+    res.status(201).send({
+      data: result, error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: null,
+      error: true
+    })
+  }
+}
+
+export const getUOM = async (
+  _req: NextApiRequest,
+  res: NextApiResponse<response<GetUOM[]>>
+): Promise<void> => {
+  try {
+    const result = await prisma.$queryRaw<GetUOM[]>`SELECT 
+units_of_measure_types.id,units_of_measure_types.name as uomt_name,
+JSON_ARRAYAGG(JSON_OBJECT(
+'name', units_of_measure.name,
+'abbreviation', units_of_measure.abbreviation
+)) AS uom
+FROM units_of_measure_types
+INNER JOIN units_of_measure ON units_of_measure_types.id = units_of_measure.uomt_id
+GROUP BY uomt_name
+`
+    res.status(200).send({
+      data: result,
+      error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: null,
+      error: true
+    })
+  }
+}
+
+export const updateUOM = async (
+  req: CreateUOM,
+  res: NextApiResponse<response<string>>
+): Promise<void> => {
+  try {
+    const { abbreviation, name, uomt_id, creation_date } = req.body
+    unitOfMeasureValidations({
+      abbreviation,
+      creationDate: creation_date as unknown as string,
+      name,
+      uomtId: uomt_id
+    })
+    await prisma.$executeRaw`UPDATE IGNORE units_of_measure SET
+name = ${name},
+abbreviation = ${abbreviation},
+uomt_id = ${uomt_id}`
+    res.status(200).send({
+      data: 'successful update',
+      error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: null,
+      error: true
+    })
+  }
+}
+
+interface CreateUOM extends NextApiRequest {
+  body: units_of_measure
+}
+
+interface GetUOM {
+  id: number
+  uomt_name: string
+  uom: Array<{
+    name: string
+    abbreviation: string
+  }>
+}
---
+++ src/pages/api/uom.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createUOM, getUOM, updateUOM } from 'controllers/food_organizer_crud/unitsOfMeasureCRUD'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const { method } = req
+  switch (method) {
+    case 'POST':
+      await createUOM(
+        req,
+        res
+      )
+      break
+    case 'GET':
+      await getUOM(
+        req,
+        res
+      )
+      break
+    case 'PATCH':
+      await updateUOM(
+        req,
+        res
+      )
+      break
+    default:
+      res.setHeader(
+        'Allow',
+        [
+          'POST',
+          'GET',
+          'PATCH'
+        ]
+      )
+      res.status(405).send({
+        data: null,
+        error: true
+      })
+  }
+}
+
+export default handler
