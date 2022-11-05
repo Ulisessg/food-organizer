42eeb4cedd43e197c65a5cf7346f38bfd08fa887
Ulises Antonio <ukisessg@gmail.com>
2022-11-04 20:57:20 -0600

    Daily Menu Price CRU created

---
+++ src/controllers/food_organizer_crud/dailyMenuPriceCRUD.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable camelcase */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import priceValidations, { validations } from 'models/priceValidations'
+import type { daily_menu_prices } from '@prisma/client'
+import prisma from 'lib/prisma'
+import { response } from 'controllers/response'
+
+export const createDailyMenuPrice = async (
+  req: CreateDailyMenuPrice,
+  res: NextApiResponse<response<string>>
+): Promise<void> => {
+  try {
+    const { creation_date, menu_id, price_date, value } = req.body
+    priceValidations({
+      creationDate: creation_date as unknown as string,
+      id: menu_id,
+      idName: 'dailyMenuPrice',
+      priceDate: price_date as unknown as string,
+      value: value as unknown as number
+    })
+    await prisma.daily_menu_prices.create({
+      data: { ...req.body }
+    })
+    res.status(201).send({
+      data: 'daily menu price created',
+      error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: 'error creating daily menu foods',
+      error: true
+    })
+  }
+}
+
+export const getDailyMenuPrices = async (
+  _req: CreateDailyMenuPrice,
+  res: NextApiResponse< response<daily_menu_prices[] | string>>
+): Promise<void> => {
+  try {
+    const result = await prisma.daily_menu_prices.findMany()
+    res.status(200).send({
+      data: result,
+      error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: 'error getting daily menu foods',
+      error: true
+    })
+  }
+}
+export const updateDailyMenuPrice = async (
+  req: CreateDailyMenuPrice,
+  res: NextApiResponse< response<daily_menu_prices[] | string>>
+): Promise<void> => {
+  try {
+    const { id, menu_id, price_date, value } = req.body
+    validations.id(
+      menu_id,
+      'dailyMenuPrice'
+    )
+    validations.price(value as unknown as number)
+    validations.priceDate(price_date as unknown as string)
+    await prisma.daily_menu_prices.update({
+      data: {
+        menu_id,
+        price_date,
+        value
+      },
+      where: {
+        id
+      }
+    })
+    res.status(200).send({
+      data: 'daily menu price updated',
+      error: false
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      data: 'error updating daily menu foods',
+      error: true
+    })
+  }
+}
+
+interface CreateDailyMenuPrice extends NextApiRequest {
+  body: daily_menu_prices
+}
---
+++ src/pages/api/dailymenuprice.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import {
+  createDailyMenuPrice, getDailyMenuPrices, updateDailyMenuPrice
+} from 'controllers/food_organizer_crud/dailyMenuPriceCRUD'
+import { response } from 'controllers/response'
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<response<string | any>>
+): Promise<void> => {
+  const { method } = req
+
+  switch (method) {
+    case 'POST':
+      await createDailyMenuPrice(
+        req,
+        res
+      )
+      break
+    case 'GET':
+      await getDailyMenuPrices(
+        req,
+        res
+      )
+      break
+    case 'PATCH':
+      await updateDailyMenuPrice(
+        req,
+        res
+      )
+      break
+    default:
+      res.setHeader(
+        'Allow',
+        [
+          'POST',
+          'GET',
+          'PATCH'
+        ]
+      )
+      res.status(405).send({
+        data: 'method not allowed',
+        error: false
+      })
+  }
+}
+
+export default handler
