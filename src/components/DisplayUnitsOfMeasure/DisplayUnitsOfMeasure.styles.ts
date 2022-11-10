c663d3dc09503b6432eb21d210cccb609f10e7aa
Ulises Antonio <ukisessg@gmail.com>
2022-11-09 19:32:18 -0600

    Display Units Of Measure styles fixed: text capitalized

--- src/components/DisplayUnitsOfMeasure/DisplayUnitsOfMeasure.styles.ts
+++ src/components/DisplayUnitsOfMeasure/DisplayUnitsOfMeasure.styles.ts
@@ -20,6 +20,7 @@ box-shadow: 2px 2px 6px 2px ${({ theme }) => theme.colors.shadow};
 margin: 50px 0;
 border: 2px solid ${({ theme }) => theme.colors.shadow};
 border-radius: 20px;
+text-transform: capitalize;
 @media screen  and (min-width:  1025px  ){
   width: 85vw;
 }
