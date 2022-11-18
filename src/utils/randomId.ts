9d7518761ab09de06b6ea80df601b959cdb7abfd
Ulises Antonio <ukisessg@gmail.com>
2022-11-18 01:36:46 -0600

    RandomId util fixed

--- src/utils/randomId.ts
+++ src/utils/randomId.ts
@@ -1,3 +1,4 @@
-const randomId = (): string => `${Math.floor((Math.random() * 33) * Math.random() * 1224)}`
+const randomId = ():
+string => `${Math.floor((Math.random() * 33) * (Math.random() * 1224) * (Math.random() * 7327))}`
 
 export default randomId
