5c17fd1768e4a854b1169d2a6839200bd1d6737e
Ulises Antonio <ukisessg@gmail.com>
2022-11-17 19:08:29 -0600

    'Create purchase place' component added

---
+++ src/components/CreatePurchasePlace.tsx
@@ -0,0 +1,34 @@
+import { Button, Form, TextInput } from 'd-system'
+import React, { FC } from 'react'
+import Details from 'components/common/Details'
+
+const CreatePurchasePlace: FC = () => <>
+  <Details summary="Crear lugar de compra">
+  <Form title="Crear lugar de compra">
+    <TextInput
+      id="name"
+      inputMode="text"
+      label="Nombre del luugar de compra"
+      name="address"
+      placeholder="Tianguis, mercado..."
+      type="text"
+    />
+    <TextInput
+      id="address"
+      inputMode="text"
+      label="Dirección (opcional)"
+      name="address"
+      placeholder="Benito López 24"
+      type="text"
+    />
+    <Button
+      colorMessage="continue"
+      size="100%"
+      text="Añadir nueva dirección"
+      type="button"
+    />
+  </Form>
+  </Details>
+</>
+
+export default CreatePurchasePlace
