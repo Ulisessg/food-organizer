ff3654eef1cb658445cc58ea28d22ab2efe57ab6
Ulises Antonio <ukisessg@gmail.com>
2023-02-12 00:37:06 -0600

    Random id complexity inreased

--- src/utils/randomId.ts
+++ src/utils/randomId.ts
@@ -1,4 +1,14 @@
-const randomId = ():
-string => `${Math.floor((Math.random() * 33) * (Math.random() * 1224) * (Math.random() * 7327))}`
+/**
+ *
+ * @param {string} prefix - Value used before random numbers
+ * @returns
+ */
+const randomId = (prefix?: string):
+string => `${prefix ?? ''}${Math.floor((
+  Math.random() * 343534453) *
+  (Math.random() * 12242435353) *
+   (Math.random() * 343237453423) *
+   (Math.random() * 343237327) *
+   (Math.random() * 344323212346))}`
 
 export default randomId
