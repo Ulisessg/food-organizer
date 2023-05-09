90a1b2b700e60ae182eab8f6c8f9407c5445a1a0
Ulises Antonio <ukisessg@gmail.com>
2023-05-09 04:35:45 -0600

    Header titles fixed in RN

--- native/index.tsx
+++ native/index.tsx
@@ -1,14 +1,20 @@
 import React, { type FC } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { Text } from 'react-native'
 import pageStyles from 'styles/pages/pageStyles'
 
 const Index: FC = () => (
+  <>
+    <Stack.Screen options={{
+      header: () => null
+    }} />
     <SafeAreaView style={pageStyles.container}>
       <Text>Home</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
+  </>
 )
 
 export default Index
--- native/settings.tsx
+++ native/settings.tsx
@@ -1,14 +1,20 @@
 import React, { type FC } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { Text } from 'react-native'
 import pageStyles from 'styles/pages/pageStyles'
 
 const Config: FC = () => (
+  <>
+    <Stack.Screen options={{
+      headerTitle: 'Configuración'
+    }} />
     <SafeAreaView style={pageStyles.container}>
       <Text>Configruración</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
+  </>
 )
 
 // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
