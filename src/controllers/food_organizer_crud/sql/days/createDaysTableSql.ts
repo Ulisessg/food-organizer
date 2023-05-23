403d6e043437440a870d8f7f05766c4a1c7063ea
Ulises Antonio <ukisessg@gmail.com>
2023-05-23 06:24:24 -0600

    Unnecesary database insertion in 'days' table removed

--- src/controllers/food_organizer_crud/sql/days/createDaysTableSql.ts
+++ src/controllers/food_organizer_crud/sql/days/createDaysTableSql.ts
@@ -6,14 +6,6 @@ const createDaysTableSql = `
     id INTEGER PRIMARY KEY,
     name CHAR(9) NOT NULL UNIQUE
   );
-  INSERT OR IGNORE INTO days (id,name) VALUES
-  (NULL,"Lunes"),
-  (NULL,"Martes"),
-  (NULL,"Miercoles"),
-  (NULL,"Jueves"),
-  (NULL,"Viernes"),
-  (NULL,"Sábado"),
-  (NULL,"Domingo");
 `
 
 export default createDaysTableSql
