e6ed87bea1e35baafc7f5cb8457552e81bbad39b
Ulises Antonio <ukisessg@gmail.com>
2023-06-28 18:20:07 -0600

    UseGetRequest hook deleted

--- src/pages/admin/week.tsx
+++ src/pages/admin/week.tsx
@@ -1,26 +1,11 @@
 import CreateWeeklyMenu from 'components/web/CreateWeeklyMenu'
 import DisplayWeeklyMenus from 'components/web/DisplayWeeklyMenus'
-import ErrorMessage from 'components/web/common/ErrorMessage'
-import {
-  type GetWeeklyMenu
-} from 'controllers/food_organizer_crud/sql/weeklyMenus/getWeeklyMenusSql'
 import Head from 'next/head'
-import { LoadingSpinner } from 'd-system'
 import type { NextPage } from 'next'
 import React from 'react'
 import Title from 'components/web/common/Title'
-import useGetRequest from 'hooks/useGetRequest'
 
-const WeeklyMenus: NextPage = () => {
-  const { data, error, isLoading } = useGetRequest<GetWeeklyMenu>('/api/weeklymenu')
-
-  if (isLoading) {
-    return <LoadingSpinner size="large"/>
-  }
-  if (error) {
-    return <ErrorMessage message={data as string} action="intenta de nuevo mas tarde" />
-  }
-  return <>
+const WeeklyMenus: NextPage = () => <>
   <Head>
     <title>Administrar los menús de la semana</title>
   </Head>
@@ -28,6 +13,5 @@ const WeeklyMenus: NextPage = () => {
   <CreateWeeklyMenu />
   <DisplayWeeklyMenus />
 </>
-}
 
 export default WeeklyMenus
